5f68774b070a582bd68af72bc081fb76ceee9f51
mdhossin <mdsahadathossin778@gmail.com>
2022-09-26 07:59:35 +0600

    change token expire time

--- server/config/generateToken.js
+++ server/config/generateToken.js
@@ -9,7 +9,7 @@ exports.generateActiveToken = (payload) => {
 
 exports.generateAccessToken = (payload) => {
   return jwt.sign(payload, `${process.env.ACCESS_TOKEN_SECRET}`, {
-    expiresIn: "15m",
+    expiresIn: "5m",
   });
 };
 
